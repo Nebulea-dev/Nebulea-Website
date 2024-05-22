079c81abc5b49a1bec0192c86342a906785b9065
Maxence Mathieu <Sans.Plague@gmail.com>
2024-05-22 04:47:28 +0200

    renamed sumfetch to neofetch and fix emojis

--- src/utils/bin/neofetch.ts
+++ src/utils/bin/neofetch.ts
@@ -1,15 +1,15 @@
 import config from '../../../config.json';
 
-const sumfetch = async (args: string[]): Promise<string> => {
+const neofetch = async (args: string[]): Promise<string> => {
     return `
-                 R RR RR                   sumfetch
+                 R RR RR                   neofetch
               R RRRRRRRR R          R     -----------
  R RR       R RRRRRRRRRRRRR R      RR      ${config.name}
-rR RRR    R RRRRRRRRRRRRRRRRR R   RRR R    <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
-RRR RR   RRRRRRRRRRRRRRRRRRRRRRR  RRRRR   爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+rR RRR    R RRRRRRRRRRRRRRRRR R   RRR R   󰉨 <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
+RRR RR   RRRRRRRRRRRRRRRRRRRRRRR  RRRRR   󰖟 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
  RRRRR  RRRRRRRRRRRRRRRRRRRRRRRR  RRRR    -----------
-  RRR RRRRRRRRRRRRRRRRRRRRRRRRRRRR RR      CONTACT
-    R  RRRRRRRRRR=  RR = RRRRRRRRRRR       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  RRR RRRRRRRRRRRRRRRRRRRRRRRRRRRR RR     󰁥 CONTACT
+    R  RRRRRRRRRR=  RR = RRRRRRRRRRR      󰇰 <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
      RRRRRRRRRRRR=  RR = RRRRRRRRRR        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
       RRRRRRRRRRR   RR   RRRRRRRRRR        <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
      RR==RRRRRRRRRRRRRRRRRRRRRR===RR      -----------
@@ -21,4 +21,4 @@ RRR RR   RRRRRRRRRRRRRRRRRRRRRRR  RRRRR   爵 <u><a href="${config.repo}" target
 `;
 };
 
-export default sumfetch;
+export default neofetch;
